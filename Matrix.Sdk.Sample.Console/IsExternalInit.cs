75f152a11cdafbd15b36fd8f9aaad7547ecad9d3
Mich4el <Mich4el@protonmail.com>
2021-12-03 13:03:55 +0300

    Create IsExternalInit.cs

---
+++ Matrix.Sdk.Sample.Console/IsExternalInit.cs
@@ -0,0 +1,10 @@
+// ReSharper disable once CheckNamespace
+// This need for Rider to properly work with records
+
+namespace System.Runtime.CompilerServices
+{
+    using ComponentModel;
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public record IsExternalInit;
+}
